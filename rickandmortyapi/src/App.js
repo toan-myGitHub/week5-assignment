c86debc9c04593453f23bc89d5731ae325af8356
toanhbui <toanhbui@uw.edu>
2019-05-12 22:54:48 -0700

    tony

--- rickandmortyapi/src/App.js
+++ rickandmortyapi/src/App.js
@@ -1,13 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+super(props);
+this.state = {data: {},
+isLoading: true
+
+}
+  }
+
+  componentDidMount() {
+    fetch('https://rickandmortyapi.com/api/')
+      .then(response => {
+        return response.json();
+      })
+      .then(data =>  {
+        this.setState({
+          data: data.message, 
+          isLoading: false
+        });
+
+      }
+      );
+
+
+  }
+
+
+render () {
+
+if (this.state.isLoading) {
+return <div>loading ...</div>
+}
+
+
+  const breeds = Object.keys(this.state.data);
+  breeds.map((breed, idx) => <li key={idx}>{breed}</li>);
+
+{/*
+  
+  console.log(breeds);
+*/}
+
+
+
   return (
     <div>
+
 <h2>Rick and Morty API</h2>
+{/*
+
+  {JSON.stringify(this.state)}
+*/}
+<ul>
+
+
+    {breeds}
+
+
+</ul>
+    
+
     </div>
   );
+}
 }
 
 export default App;
